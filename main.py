ed3dcacff91187b6ba93e5cee6e7be811a613470
Finn Alberts <finnalberts@gmail.com>
2022-10-19 17:59:56 +0200

    Update app to use trained model
    The code for training is  commented out here.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gym
 from environment import TetrisEnvironment
 from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
 from os import path
 from graph import graph
 from multiprocessing import Process
@@ -20,20 +21,32 @@ def main():
     print("START TRAINING")
     # Register Gym environment and create model
     gym.register('Tetris-v0', entry_point=TetrisEnvironment)
-    model = PPO('MlpPolicy', 'Tetris-v0')
 
     # Check if a model already exists and if so load it
     if path.exists("trained_model.zip"):
-        model.load("trained_model.zip")
+        model = PPO.load("trained_model.zip")
+    else:
+        model = PPO('MlpPolicy', 'Tetris-v0', verbose=1)
 
     # Run the program infinitely
     while True:
         # Learn for 20 000 steps
-        model.learn(20000)
+        # model.learn(20000)
 
         # Save the model
         print("Saving the model to trained_model.zip")
-        model.save("trained_model.zip")
+        # model.save("trained_model.zip")
+
+        # Try out trained model
+        environment = TetrisEnvironment()
+        obs = environment.reset()
+        for i in range(100000):
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, info = environment.step(action)
+            if done:
+              obs = environment.reset()
+
+        environment.close()
 
 
 if __name__ == "__main__":

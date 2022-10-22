17d9ef0c69749b6bcb1968ad870d2b0efeeafbc3
Finn Alberts <finnalberts@gmail.com>
2022-10-22 15:30:30 +0200

    Remove loading of model

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ def main():
     # Register Gym environment and create model
     gym.register('Tetris-v0', entry_point=TetrisEnvironment)
 
-    # Check if a model already exists and if so load it
-    if path.exists("trained_model.zip"):
-        model = PPO.load("trained_model.zip")
-    else:
-        model = PPO('MlpPolicy', 'Tetris-v0', verbose=1)
 
     # Run the program infinitely
     while True:
@@ -26,15 +21,5 @@ def main():
         print("Saving the model to trained_model.zip")
         # model.save("trained_model.zip")
 
-        # Try out trained model
-        environment = TetrisEnvironment()
-        obs = environment.reset()
-        for i in range(100000):
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, done, info = environment.step(action)
-            if done:
-              obs = environment.reset()
-
-        environment.close()
 
 

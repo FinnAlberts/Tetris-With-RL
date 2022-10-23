54abefb703188f1e500dadac375f8fdffad5bce9
Finn Alberts <finnalberts@gmail.com>
2022-10-23 13:09:16 +0200

    Cleanup code and add commenting

--- check_enviroment.py
+++ check_enviroment.py
@@ -1,14 +1,17 @@
+# This is a script for checking the environment by executing random actions
+# The action, observation and reward variables are printed to the console
+
 from environment import TetrisEnvironment
 
-env = TetrisEnvironment()
+environment = TetrisEnvironment()
 episodes = 50
 
 for episode in range(episodes):
     done = False
-    obs = env.reset()
-    while True:#not done:
-        random_action = env.action_space.sample()
+    observation = environment.reset()
+    while True:
+        random_action = environment.action_space.sample()
         print("action", random_action)
-        obs, reward, done, info = env.step(random_action)
-        print('obs', obs)
+        obs, reward, done, info = environment.step(random_action)
+        print('observation', obs)
         print('reward', reward)
